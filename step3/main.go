206bacdd77763351df69da5380e0b90d7e95a63c
boyang <Junbo.Yang@watchguard.com>
2019-07-15 15:47:29 +0800

    step3

---
+++ step3/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/shadow1163/logger"
+	"github.com/shadow1163/nes-go/step3/nes"
+)
+
+func main() {
+	log := logger.NewLogger()
+	flag.Parse()
+
+	var args []string = flag.Args()
+
+	if len(args) != 1 {
+		fmt.Println("Usage: nes FILENAME.ROM")
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+	cart, err := nes.LoadCartridge(args[0])
+	if err != nil {
+		log.Error(err)
+		os.Exit(1)
+	}
+	cpu := nes.NewCPU(cart)
+	cpu.PC = 0xc000
+	for i := 0; i < 1000; i++ {
+		cpu.Step()
+	}
+	// log.Info(fmt.Sprintf("$%04x", cpu.Read16(0xFFFA)))
+	// log.Info(fmt.Sprintf("$%04x", cpu.Read16(0xFFFC)))
+	// log.Info(fmt.Sprintf("$%04x", cpu.Read16(0xFFFE)))
+	// log.Info(fmt.Sprintf("%2x", cpu.Read(0x6000)))
+	// cpu.PC = cpu.Read16(0xFFFA)
+	// cpu.PrintInstruction()
+	// cpu.PC = cpu.Read16(0xFFFC)
+	// cpu.PrintInstruction()
+	// cpu.PC = cpu.Read16(0xFFFE)
+	// cpu.PrintInstruction()
+}
